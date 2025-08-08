aa1af490f55fa9f71f13fb21254311952dc4645b
MinhNhutDev <klemin3005@gmail.com>
2025-08-08 10:54:17 +0700

    Function:  Load Danh Sach San Pham

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,12 @@
-module.exports.index = (req, res) => {
-    res.render("./client/pages/products/index.pug",{
-        titlePage:"Trang danh sách sản phẩm"
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+        status: "active"
+    })  //- tim tat ca 
 
+    res.render("./client/pages/products/index.pug",{
+        titlePage:"Trang danh sách sản phẩm",
+        products: products
     });
 }
--- models/product.model.js
+++ models/product.model.js
@@ -4,7 +4,7 @@ const ProductSchema = new mongoose.Schema({
     title: String,
     description: String,
     category: String,
-    price: Double,
+    price: Number,
     brand: String,
     images: [String],
     thumbnail: String,
