ad6ea502397a19a4aefa2b25ff6c736387293849
MinhNhutDev <klemin3005@gmail.com>
2025-08-08 19:36:51 +0700

    Function: Tim Kiem San Pham

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,20 @@ if (buttonFillerStatus) {
         })
     })
 }
+
+
+const formSearch = document.querySelector("#formSearch");
+if (formSearch){
+    const url = new URL(window.location.href);
+    formSearch.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+        if (keyword){
+            url.searchParams.set("keyword",keyword);
+        }
+        else{
+             url.searchParams.delete("keyword");
+        }
+        window.location.href = url;
+    })
+}
