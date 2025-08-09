d4aa19519485f4d719de662d0d1a4311e6e0a64a
MinhNhutDev <klemin3005@gmail.com>
2025-08-09 16:38:59 +0700

    Toi uu bo loc tim kiem

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,55 +1,30 @@
 // [GET] admin/products
 const Prodcut = require("../../models/product.model");
+const helperFillterStatus = require("../../helpers/fillterStatus");
+const helperSearch = require("../../helpers/search");
+
 module.exports.index = async (req, res) => {
 
-    let fillerButton = [
-        {
-            name:"Tất cả",
-            class: "active",
-            status:""
-        },
-        {
-            name:"Đang hoạt động",
-            class: "",
-            status:"active"
-        },
-        {
-            name:"Ngưng hoạt động",
-            class: "",
-            status:"inactive"
-        }
-    ]
-
-    
-
-    if (req.query.status){
-        const index = fillerButton.findIndex((btn)=>btn.status == req.query.status);
-        fillerButton[index].class = "btn-success";
-    }
-    else{
-        const index = fillerButton.findIndex((btn)=>btn.status == "");
-        fillerButton[index].class = "btn-success";
-    }
+    const fillterStatus = helperFillterStatus(req.query);
 
     let find = {
         deleted: false,
     }
 
-    
     if (req.query.status){
         find.status = req.query.status;
     }
 
-    let keyword = "";
-    if (req.query.keyword){
-        keyword=req.query.keyword;
-        find.title = { $regex: keyword, $options: "i" };
+    const objectSearch = helperSearch(req.query);
+
+    if (objectSearch.regex){
+        find.title = objectSearch.regex;
     }
 
     const products = await Prodcut.find(find);
     res.render("./admin/pages/products/products.pug",{
         productsList:products,
-        fillerButton:fillerButton,
-        keyword: keyword,
+        fillerButton:fillterStatus,
+        keyword: objectSearch.keyword,
     });
 }
---
+++ helpers/search.js
@@ -0,0 +1,10 @@
+module.exports = (query)=>{
+    let objectSearch = {
+        keyword :"",
+    }
+    if (query.keyword){
+        objectSearch.keyword = query.keyword;
+        objectSearch.regex = { $regex: objectSearch.keyword, $options: "i" };
+    }
+    return objectSearch;
+}
