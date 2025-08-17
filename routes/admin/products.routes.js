572ee9222aee07ae1c4b370b43382de8f2493b4b
MinhNhutDev <klemin3005@gmail.com>
2025-08-17 19:26:44 +0700

    Chuc nang upload anh

--- routes/admin/products.routes.js
+++ routes/admin/products.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../../controllers/admin/products.controller");
+const multer  = require('multer')
+const storageMulterHelper = require('../../helpers/storageMulter');
+const upload = multer({ storage: storageMulterHelper() })
 
 router.get("/",controller.index);
 router.patch("/change-status/:status/:id",controller.changeStatus);
@@ -9,5 +12,5 @@ router.delete("/deleteItem/:id",controller.deleteItem);
 router.get("/trash",controller.trash);
 router.patch("/trash/restore/:id",controller.restore);
 router.get("/create",controller.create);
-router.post("/create",controller.createPost);
+router.post("/create",upload.single('image'),controller.createPost);
 module.exports = router;
