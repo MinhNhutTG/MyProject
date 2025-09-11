c621b758b2c1861a86baae6b9a8bb76703f00483
MinhNhutDev <klemin3005@gmail.com>
2025-09-11 13:38:48 +0700

    PermissionP2

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -21,6 +21,8 @@ module.exports.createPost = async (req,res)=>{
 
     await role.save();
 
+    req.flash("Thêm quyền thành công!")
+
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
 
@@ -37,6 +39,7 @@ module.exports.edit = async (req,res)=>{
 module.exports.editPost = async (req,res)=>{
     const id = req.params.id
     await Role.updateOne({_id:id},req.body)
+    req.flash("Cập nhật quyền thành công!")
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
 
@@ -46,5 +49,32 @@ module.exports.delete = async (req,res)=>{
     await Role.updateOne({_id:id},{
         deleted: true,
         deleteAt: new Date().toLocaleString()});
+    req.flash("Xóa quyền thành công!")
     res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+
+
+module.exports.permission =  async (req,res)=>{
+ 
+    let find = {
+        deleted:false,
+    }
+    const record = await Role.find(find);
+    res.render("./admin/pages/roles/permission.pug",{
+        record:record
+    })
+}
+
+module.exports.permissionPatch = async (req,res)=>{
+
+    const permission = JSON.parse(req.body.permission)
+    
+    for (const item of permission){
+        await Role.updateOne({_id:item.id},{permission: item.permission})
+    }
+
+    req.flash("Cập nhật phân quyền thành công!");
+
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
+
 }
